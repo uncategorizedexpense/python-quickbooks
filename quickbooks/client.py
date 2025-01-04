4737f87c72bf4301a89f95630283a6b44db9b6bc
jaredthecoder <jared@jaredsmith.io>
2025-01-04 13:30:34 -0500

    Fix fault not appearing due to string conversion on dict

--- quickbooks/client.py
+++ quickbooks/client.py
@@ -4,10 +4,12 @@ try:  # Python 3
     import http.client as httplib
     from urllib.parse import parse_qsl
     from functools import partial
+
     to_bytes = lambda value, *args, **kwargs: bytes(value, "utf-8", *args, **kwargs)
 except ImportError:  # Python 2
     import httplib
     from urlparse import parse_qsl
+
     to_bytes = str
 
 import textwrap
@@ -28,8 +30,8 @@ except ImportError:
 
 
 class Environments(object):
-    SANDBOX = 'sandbox'
-    PRODUCTION = 'production'
+    SANDBOX = "sandbox"
+    PRODUCTION = "production"
 
 
 class QuickBooks(object):
@@ -46,14 +48,41 @@ class QuickBooks(object):
     current_user_url = "https://appcenter.intuit.com/api/v1/user/current"
 
     _BUSINESS_OBJECTS = [
-        "Account", "Attachable", "Bill", "BillPayment",
-        "Class", "CreditMemo", "Customer", "CustomerType", "CompanyCurrency",
-        "Department", "Deposit", "Employee", "Estimate", "ExchangeRate", "Invoice",
-        "Item", "JournalEntry", "Payment", "PaymentMethod", "Preferences",
-        "Purchase", "PurchaseOrder", "RefundReceipt",
-        "SalesReceipt", "TaxAgency", "TaxCode", "TaxService/Taxcode", "TaxRate", "Term",
-        "TimeActivity", "Transfer", "Vendor", "VendorCredit", "CreditCardPayment",
-        "RecurringTransaction"
+        "Account",
+        "Attachable",
+        "Bill",
+        "BillPayment",
+        "Class",
+        "CreditMemo",
+        "Customer",
+        "CustomerType",
+        "CompanyCurrency",
+        "Department",
+        "Deposit",
+        "Employee",
+        "Estimate",
+        "ExchangeRate",
+        "Invoice",
+        "Item",
+        "JournalEntry",
+        "Payment",
+        "PaymentMethod",
+        "Preferences",
+        "Purchase",
+        "PurchaseOrder",
+        "RefundReceipt",
+        "SalesReceipt",
+        "TaxAgency",
+        "TaxCode",
+        "TaxService/Taxcode",
+        "TaxRate",
+        "Term",
+        "TimeActivity",
+        "Transfer",
+        "Vendor",
+        "VendorCredit",
+        "CreditCardPayment",
+        "RecurringTransaction",
     ]
 
     __instance = None
@@ -70,11 +99,11 @@ class QuickBooks(object):
         else:
             instance = object.__new__(cls)
 
-        if 'refresh_token' in kwargs:
-            instance.refresh_token = kwargs['refresh_token']
+        if "refresh_token" in kwargs:
+            instance.refresh_token = kwargs["refresh_token"]
 
-        if 'auth_client' in kwargs:
-            instance.auth_client = kwargs['auth_client']
+        if "auth_client" in kwargs:
+            instance.auth_client = kwargs["auth_client"]
 
             if instance.auth_client.environment == Environments.SANDBOX:
                 instance.sandbox = True
@@ -84,16 +113,16 @@ class QuickBooks(object):
             refresh_token = instance._start_session()
             instance.refresh_token = refresh_token
 
-        if 'company_id' in kwargs:
-            instance.company_id = kwargs['company_id']
+        if "company_id" in kwargs:
+            instance.company_id = kwargs["company_id"]
 
-        if 'minorversion' in kwargs:
-            instance.minorversion = kwargs['minorversion']
+        if "minorversion" in kwargs:
+            instance.minorversion = kwargs["minorversion"]
 
-        instance.invoice_link = kwargs.get('invoice_link', False)
+        instance.invoice_link = kwargs.get("invoice_link", False)
 
-        if 'verifier_token' in kwargs:
-            instance.verifier_token = kwargs.get('verifier_token')
+        if "verifier_token" in kwargs:
+            instance.verifier_token = kwargs.get("verifier_token")
 
         return instance
 
@@ -121,8 +150,8 @@ class QuickBooks(object):
     def validate_webhook_signature(self, request_body, signature, verifier_token=None):
         hmac_verifier_token_hash = hmac.new(
             to_bytes(verifier_token or self.verifier_token),
-            request_body.encode('utf-8'),
-            hashlib.sha256
+            request_body.encode("utf-8"),
+            hashlib.sha256,
         ).digest()
         decoded_hex_signature = base64.b64decode(signature)
         return hmac_verifier_token_hash == decoded_hex_signature
@@ -138,7 +167,9 @@ class QuickBooks(object):
         if qs is None:
             qs = {}
 
-        url = self.api_url + "/company/{0}/reports/{1}".format(self.company_id, report_type)
+        url = self.api_url + "/company/{0}/reports/{1}".format(
+            self.company_id, report_type
+        )
         result = self.get(url, params=qs)
         return result
 
@@ -150,48 +181,59 @@ class QuickBooks(object):
         result = self.get(url, params=params)
         return result
 
-    def make_request(self, request_type, url, request_body=None, content_type='application/json',
-                     params=None, file_path=None, request_id=None):
+    def make_request(
+        self,
+        request_type,
+        url,
+        request_body=None,
+        content_type="application/json",
+        params=None,
+        file_path=None,
+        request_id=None,
+    ):
 
         if not params:
             params = {}
 
         if self.minorversion:
-            params['minorversion'] = self.minorversion
-        
+            params["minorversion"] = self.minorversion
+
         if request_id:
-            params['requestid'] = request_id
+            params["requestid"] = request_id
 
         if self.invoice_link:
-            params['include'] = 'invoiceLink'
+            params["include"] = "invoiceLink"
 
         if not request_body:
             request_body = {}
 
         headers = {
-            'Content-Type': content_type,
-            'Accept': 'application/json',
-            'User-Agent': 'python-quickbooks V3 library'
+            "Content-Type": content_type,
+            "Accept": "application/json",
+            "User-Agent": "python-quickbooks V3 library",
         }
 
         if file_path:
-            attachment = open(file_path, 'rb')
-            url = url.replace('attachable', 'upload')
-            boundary = '-------------PythonMultipartPost'
-            headers.update({
-                'Content-Type': 'multipart/form-data; boundary=%s' % boundary,
-                'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                'User-Agent': 'python-quickbooks V3 library',
-                'Accept': 'application/json',
-                'Connection': 'close'
-            })
+            attachment = open(file_path, "rb")
+            url = url.replace("attachable", "upload")
+            boundary = "-------------PythonMultipartPost"
+            headers.update(
+                {
+                    "Content-Type": "multipart/form-data; boundary=%s" % boundary,
+                    "Accept-Encoding": "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+                    "User-Agent": "python-quickbooks V3 library",
+                    "Accept": "application/json",
+                    "Connection": "close",
+                }
+            )
 
-            binary_data = str(base64.b64encode(attachment.read()).decode('ascii'))
+            binary_data = str(base64.b64encode(attachment.read()).decode("ascii"))
 
-            content_type = json.loads(request_body)['ContentType']
+            content_type = json.loads(request_body)["ContentType"]
 
-            request_body = textwrap.dedent(
-                """
+            request_body = (
+                textwrap.dedent(
+                    """
                 --%s
                 Content-Disposition: form-data; name="file_metadata_01"
                 Content-Type: application/json
@@ -207,27 +249,47 @@ class QuickBooks(object):
 
                 --%s--
                 """
-            ) % (boundary, request_body, boundary, content_type, binary_data, boundary)
+                )
+                % (
+                    boundary,
+                    request_body,
+                    boundary,
+                    content_type,
+                    binary_data,
+                    boundary,
+                )
+            )
 
             # make sure request_body is not unicode (python 2 case)
             request_body = str(request_body)
 
-        req = self.process_request(request_type, url, headers=headers, params=params, data=request_body)
+        req = self.process_request(
+            request_type, url, headers=headers, params=params, data=request_body
+        )
 
         if req.status_code == httplib.UNAUTHORIZED:
             raise exceptions.AuthorizationException(
-                "Application authentication failed", error_code=req.status_code, detail=req.text)
+                "Application authentication failed",
+                error_code=req.status_code,
+                detail=req.text,
+            )
 
         try:
             result = req.json()
         except:
-            raise exceptions.QuickbooksException("Error reading json response: {0}".format(req.text), 10000)
+            raise exceptions.QuickbooksException(
+                "Error reading json response: {0}".format(req.text), 10000
+            )
 
-        if "Fault" in result:
+        if "Fault" in result or "Fault" in str(result):
             self.handle_exceptions(result["Fault"])
         elif not req.status_code == httplib.OK:
-            raise exceptions.QuickbooksException("Error returned with status code '{0}': {1}".format(
-                req.status_code, req.text), 10000)
+            raise exceptions.QuickbooksException(
+                "Error returned with status code '{0}': {1}".format(
+                    req.status_code, req.text
+                ),
+                10000,
+            )
         else:
             return result
 
@@ -239,15 +301,18 @@ class QuickBooks(object):
 
     def process_request(self, request_type, url, headers="", params="", data=""):
         if self.session is None:
-            raise exceptions.QuickbooksException('No session manager')
+            raise exceptions.QuickbooksException("No session manager")
 
-        headers.update({'Authorization': 'Bearer ' + self.session.access_token})
+        headers.update({"Authorization": "Bearer " + self.session.access_token})
 
         return self.session.request(
-            request_type, url, headers=headers, params=params, data=data)
+            request_type, url, headers=headers, params=params, data=data
+        )
 
     def get_single_object(self, qbbo, pk):
-        url = "{0}/company/{1}/{2}/{3}/".format(self.api_url, self.company_id, qbbo.lower(), pk)
+        url = "{0}/company/{1}/{2}/{3}/".format(
+            self.api_url, self.company_id, qbbo.lower(), pk
+        )
         result = self.get(url, {})
 
         return result
@@ -286,35 +351,59 @@ class QuickBooks(object):
             else:
                 raise exceptions.QuickbooksException(message, code, detail)
 
-    def create_object(self, qbbo, request_body, _file_path=None, request_id=None, params=None):
+    def create_object(
+        self, qbbo, request_body, _file_path=None, request_id=None, params=None
+    ):
         self.isvalid_object_name(qbbo)
 
         url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id, qbbo.lower())
-        results = self.post(url, request_body, file_path=_file_path, request_id=request_id, params=params)
+        results = self.post(
+            url,
+            request_body,
+            file_path=_file_path,
+            request_id=request_id,
+            params=params,
+        )
 
         return results
 
     def query(self, select, params=None):
         url = "{0}/company/{1}/query".format(self.api_url, self.company_id)
-        result = self.post(url, select, content_type='application/text', params=params)
+        result = self.post(url, select, content_type="application/text", params=params)
 
         return result
 
     def isvalid_object_name(self, object_name):
         if object_name not in self._BUSINESS_OBJECTS:
-            raise Exception("{0} is not a valid QBO Business Object.".format(object_name))
+            raise Exception(
+                "{0} is not a valid QBO Business Object.".format(object_name)
+            )
 
         return True
 
-    def update_object(self, qbbo, request_body, _file_path=None, request_id=None, params=None):
-        url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id,  qbbo.lower())
-        result = self.post(url, request_body, file_path=_file_path, request_id=request_id, params=params)
+    def update_object(
+        self, qbbo, request_body, _file_path=None, request_id=None, params=None
+    ):
+        url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id, qbbo.lower())
+        result = self.post(
+            url,
+            request_body,
+            file_path=_file_path,
+            request_id=request_id,
+            params=params,
+        )
 
         return result
 
     def delete_object(self, qbbo, request_body, _file_path=None, request_id=None):
         url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id, qbbo.lower())
-        result = self.post(url, request_body, params={'operation': 'delete'}, file_path=_file_path, request_id=request_id)
+        result = self.post(
+            url,
+            request_body,
+            params={"operation": "delete"},
+            file_path=_file_path,
+            request_id=request_id,
+        )
 
         return result
 
@@ -324,7 +413,7 @@ class QuickBooks(object):
 
         return results
 
-    def misc_operation(self, end_point, request_body, content_type='application/json'):
+    def misc_operation(self, end_point, request_body, content_type="application/json"):
         url = "{0}/company/{1}/{2}".format(self.api_url, self.company_id, end_point)
         results = self.post(url, request_body, content_type)
 
@@ -332,15 +421,16 @@ class QuickBooks(object):
 
     def download_pdf(self, qbbo, item_id):
         if self.session is None:
-            raise exceptions.QuickbooksException('No session')
+            raise exceptions.QuickbooksException("No session")
 
         url = "{0}/company/{1}/{2}/{3}/pdf".format(
-            self.api_url, self.company_id, qbbo.lower(), item_id)
+            self.api_url, self.company_id, qbbo.lower(), item_id
+        )
 
         headers = {
-            'Content-Type': 'application/pdf',
-            'Accept': 'application/pdf, application/json',
-            'User-Agent': 'python-quickbooks V3 library'
+            "Content-Type": "application/pdf",
+            "Accept": "application/pdf, application/json",
+            "User-Agent": "python-quickbooks V3 library",
         }
 
         response = self.process_request("GET", url, headers=headers)
@@ -350,12 +440,17 @@ class QuickBooks(object):
             if response.status_code == httplib.UNAUTHORIZED:
                 # Note that auth errors have different result structure which can't be parsed by handle_exceptions()
                 raise exceptions.AuthorizationException(
-                    "Application authentication failed", error_code=response.status_code, detail=response.text)
+                    "Application authentication failed",
+                    error_code=response.status_code,
+                    detail=response.text,
+                )
 
             try:
                 result = response.json()
             except:
-                raise exceptions.QuickbooksException("Error reading json response: {0}".format(response.text), 10000)
+                raise exceptions.QuickbooksException(
+                    "Error reading json response: {0}".format(response.text), 10000
+                )
 
             self.handle_exceptions(result["Fault"])
         else:
